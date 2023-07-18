c1874b665d7bb67a26b3d02ab034578c10becdd1
Arta Yqubi <arta.dev21@gmail.com>
2023-07-18 15:04:54 +0330

    update project

--- src/components/commands/Email.tsx
+++ src/components/commands/Email.tsx
@@ -4,12 +4,15 @@ import { Wrapper } from "../styles/Output.styled";
 import { termContext } from "../Terminal";
 
 const Email: React.FC = () => {
+
   const { history, rerender } = useContext(termContext);
 
   /* ===== get current command ===== */
+
   const currentCommand = _.split(history[0], " ");
 
   if (rerender && currentCommand[0] === "email" && currentCommand.length <= 1) {
+    
     window.open("mailto:" + "artayqb@gmail.com", "_self");
   }
 
--- src/components/commands/GeneralOutput.tsx
+++ src/components/commands/GeneralOutput.tsx
@@ -1,10 +1,12 @@
 import { Wrapper } from "../styles/Output.styled";
 
 type Props = {
+  
   children: string;
 };
 
 const GeneralOutput: React.FC<Props> = ({ children }) => (
+
   <Wrapper>{children}</Wrapper>
 );
 export default GeneralOutput;
