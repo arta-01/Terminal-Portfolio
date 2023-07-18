19bd58206cc2207037d62fead06c0b734d2e28f6
Arta Yqubi <arta.dev21@gmail.com>
2023-07-18 15:06:24 +0330

    update...

--- src/components/commands/Help.tsx
+++ src/components/commands/Help.tsx
@@ -8,9 +8,12 @@ import {
 import { commands } from "../Terminal";
 import { generateTabs } from "../../utils/funcs";
 
+
 const Help: React.FC = () => {
   return (
+
     <HelpWrapper data-testid="help">
+
       {commands.map(({ cmd, desc, tab }) => (
         <CmdList key={cmd}>
           <Cmd>{cmd}</Cmd>
@@ -18,12 +21,14 @@ const Help: React.FC = () => {
           <CmdDesc>- {desc}</CmdDesc>
         </CmdList>
       ))}
+      
       <KeyContainer>
         <div>Tab or Ctrl + i&nbsp; =&gt; autocompletes the command</div>
         <div>Up Arrow {generateTabs(5)} =&gt; go back to previous command</div>
         <div>Ctrl + l {generateTabs(5)} =&gt; clear the terminal</div>
       </KeyContainer>
     </HelpWrapper>
+
   );
 };
 
