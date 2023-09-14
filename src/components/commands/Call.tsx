636499c420fe9f729408745a92aed667249e5d60
Arta Yqubi <arta.dev21@gmail.com>
2023-09-14 12:18:01 +0330

    update commands

--- src/components/commands/Call.tsx
+++ src/components/commands/Call.tsx
@@ -8,7 +8,7 @@ const Call: React.FC = () => {
     return (
         <AboutWrapper data-testid="about">
             <p>
-                
+
                 <HighlightSpan>my Numbers </HighlightSpan> :
             </p>
             <p>
